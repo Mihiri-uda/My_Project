2478065dc8985f9f80d847b4b845a6a93324aca0
Mihiri-uda <mihiriudayangani.07@gmail.com>
2020-10-07 02:00:06 +0530

    updated patterns code-3

--- patterns.py
+++ patterns.py
@@ -1,7 +1,7 @@
 def pattern(n):
     k=2* n-2
-    for i in range(0,n):
-        for j in range(0,k):
+    for i in range(1,n):
+        for j in range(k,-1,-1):
             print(end=" ")
         k = k-1
         for j in range(0, i+1):
